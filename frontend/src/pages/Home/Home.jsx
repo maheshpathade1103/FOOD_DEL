8224aac8e889aa94641c0a06247d6f2ee26ab0e2
Mahesh <maheshpathade2003@gmail.com>
2025-01-14 23:26:27 +0530

    Added the image and text content on it with the view menu button. the routing has been set up with home, cart and place order options

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+import Header from '../../components/Header/Header'
+
+const Home = () => {
+  return (
+    <div>
+        {/* We will mount the header component to out home page */}
+      <Header/>
+    </div>
+  )
+}
+
+export default Home
