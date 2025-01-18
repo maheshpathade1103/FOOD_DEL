dafe5ae2a08127b1483c50db2c65cff3adc38adc
Mahesh <maheshpathade2003@gmail.com>
2025-01-18 21:59:05 +0530

    Created function which on clicking particular category shows items related to the same

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,11 +1,33 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 export const StoreContext = createContext(null) 
 
 //create a store context provider fun
 const StoreContextProvider =  (props)=> {
+    const [cartItems, setCartItems] = useState({}) ;
+
+    // functionality for add to cart
+    const addToCart = (itemId)=>{
+        if(!cartItems[itemId]){
+             setCartItems((prev)=>({...prev, [itemId]:1}))
+        }
+        else{
+            setCartItems((prev)=>({...prev, [itemId]:prev[itemId]+1}))
+        }
+    }
+
+    //remove from cart functionality
+    const removeFromCart = (itemId)=>{
+        setCartItems((prev)=>({...prev, [itemId]:prev[itemId]-1}))
+    }
+
+
     const contextvalue = {
-        food_list
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart
     }
     return (
         <StoreContext.Provider value={contextvalue}>
