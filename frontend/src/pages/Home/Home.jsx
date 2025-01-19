b658a37dfa5ee839de32553d6589a9434ccaf2e9
Mahesh <maheshpathade2003@gmail.com>
2025-01-19 11:16:35 +0530

    responsive site...an added functionality of scrolling...when clicked on menu it will scroll to menu...clicked on mobile app then scroll to it...similarly for all

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+import AppDownload from '../../components/AppDownload/AppDownload'
 
 const Home = () => {
     //create a state function
@@ -13,6 +14,7 @@ const Home = () => {
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory}/>
       <FoodDisplay category={category}/>
+      <AppDownload/>
     </div>
   )
 }
